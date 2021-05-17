b87fb068b6be0005708832f62e82774dad06f541
hemachand <hemachandsai@gmail.com>
2021-05-17 19:25:30 +0530

    code change

--- test.ts
+++ test.ts
@@ -1,39 +1,39 @@
-function evaluateCode(inputFunction){
-    if(inputFunction(3, 2020) !== false){
+function evaluateCode(isActiveMonth){
+    if(isActiveMonth(3, 2020) !== false){
         return false
     }
     const start = new Date(2020, 3);
     const end = new Date(2020, 3);
 
-    if(inputFunction(3, 2019, start) !== false){
+    if(isActiveMonth(3, 2019, start) !== false){
         return false
     }
 
-    if(inputFunction(4, 2020, start) !== false){
+    if(isActiveMonth(4, 2020, start) !== false){
         return false
     }
 
-    if(!inputFunction(3, 2020, start)){
+    if(!isActiveMonth(3, 2020, start)){
         return false
     }
 
-    if(inputFunction(4, 2019, start) !== false){
+    if(isActiveMonth(4, 2019, start) !== false){
         return false
     }
     
-    if(inputFunction(3, 2019, null, end) !== false){
+    if(isActiveMonth(3, 2019, null, end) !== false){
         return false
     }
 
-    if(inputFunction(4, 2020, null, end) !== false){
+    if(isActiveMonth(4, 2020, null, end) !== false){
         return false
     }
 
-    if(!inputFunction(3, 2020, null, end)){
+    if(!isActiveMonth(3, 2020, null, end)){
         return false
     }
 
-    if(inputFunction(4, 2019, null, end) !== false){
+    if(isActiveMonth(4, 2019, null, end) !== false){
         return false
     }
 
